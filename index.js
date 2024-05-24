6d872bc3c0b344210cd3433e6f139ac24ad228b9
zarriy <zarriy@outlook.com>
2024-05-24 17:22:27 +0500

    good work

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const helloWorld = document.querySelector('hello world');
 console.log('updated version')
-const stats = 'we are moving in the right direction';
+
+const notFirstTime = 'Every time this happens, or anything where I should be active.';
+const notLast = 'I will be going through this pain over and over again 100th time. unless I change myself into a better';
+const howToUpdate = 'This isn"t greatness. But weakness at its fullest form.';
+const learnToChange = 'unless I change my self, this won"t be over. He has to go and say these things and looking';
+const limitedEdition = 'the edition has been full of the original content.'
+const generateIdeas = 'Ideas are special formulas to grow your businesses.'
