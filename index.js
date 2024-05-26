cd4da831b94167bad83470dcd0d13d4f3ed08771
nomanakhtar-toptal <noman.akhtar@toptal.com>
2024-05-26 17:41:18 +0500

    Revert "first commit"
    This reverts commit 1e194d50e3883c46f8fb4907fab0d4d5f6c2bcbf.
    
    ok this

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 const helloWorld = document.querySelector('hello world');
-const commitAfterReset = 'commit after reset';
-const commitBeforeRevert = 'commit before revert';
 
-const commitCheckRevert = 'commit check revert';
