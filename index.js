ea99159b20c6e56a2a663e9ef6940ace633b4b8a
noman8834 <noman@martinmonroellc.com>
2024-06-11 22:18:32 +0500

    updted

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log('updated version')
 const anotherAfterRestore = 'another after restore';
 
 const resetMethods = 'these are resets';
+const commitAfterReset = 'commit after reset';
+const commitBeforeRevert = 'commit before revert';
